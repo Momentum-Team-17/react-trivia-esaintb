e22c0adf489a238d68cfb4dadb06d42dad2a84ac
esantb <e.saint.bou@gmail.com>
2023-03-22 20:38:40 -0500

    categories, questions & answers showing up

--- src/triviaQuiz.js
+++ src/triviaQuiz.js
@@ -3,33 +3,39 @@ import axios from 'axios'
 
 export default function Quiz({ selectedCategory }) {
     const [topic, setTopic] = useState(null)
-    const [questions, setQuestions] = useState(null)
-    console.log(selectedCategory);
+    const answers = useRef(null)
     let category = useRef(null)
     let triviaQuestions = useRef(null)
 
-    // useEffect is called after each render
+
     useEffect(() => {
-        // console.log("useEffect runs")
         const URL = `https://opentdb.com/api.php?amount=10&category=${selectedCategory}&difficulty=easy`
-        // we're using JS Promises
         axios.get(URL).then((response) => {
-            // console.log(URL)
-            // console.log(response.data)
+
             setTopic(response.data)
             category.current = response.data.results[0].category
             triviaQuestions.current = response.data.results[0].question
-            // console.log(category.current)
+            let correctAnswer = response.data.results[0].correct_answer
+            let wrongAnswers = response.data.results[0].incorrect_answers
+            answers.current = [...wrongAnswers, correctAnswer]
+            // console.log(answers)
         })
     }, [])
+
+    function handleAnswer(answer) {
+
+    }
 
     if (topic) {
         return (
             <>
                 <h1> TRIVIA TIME </h1>
                 <h2> category: {category.current}  </h2>
-                <h2> {triviaQuestions.current}</h2>
-
+                <h2> {triviaQuestions.current}  </h2>
+                <ul>
+                    {answers && answers.current.map((answer, ind) =>
+                        <div key={ind} ><li onClick={() => handleAnswer(answer)}>{answer}</li></div>)}
+                </ul>
             </>
         )
     }
